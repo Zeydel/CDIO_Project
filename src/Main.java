aa0bd409d351c91660062faf27a33426d198968c
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2019-03-13 10:12:41 +0100

    Test program til prototypen som fungerer

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,60 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		Button.waitForAnyPress();
-		for(int i = 0; i < 3; i++) {
-			Sound.playTone(500, 800);
-			Delay.msDelay(200);
-		}
-
-		Sound.playTone(1000, 800);
 		
-		UnregulatedMotor b = new UnregulatedMotor(MotorPort.B);
-		UnregulatedMotor c = new UnregulatedMotor(MotorPort.C);
-		b.setPower(100);
-		c.setPower(100);
+		
+				/*
+				for(int i = 0; i < 3; i++) {
+					Sound.playTone(500, 800);
+					Delay.msDelay(200);
+				}
+				*/
 
-		b.backward();
-		c.backward();
-		Delay.msDelay(30000);
+				Sound.playTone(1000, 800);
+				
+				
+
+				//EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S1);
+				//EV3UltrasonicSensor uss = new EV3UltrasonicSensor(SensorPort.S2);
+
+				
+				/*
+				EV3LargeRegulatedMotor hjul1 = new EV3LargeRegulatedMotor(MotorPort.B);
+				EV3LargeRegulatedMotor hjul2 = new EV3LargeRegulatedMotor(MotorPort.C);
+				
+				EV3MediumRegulatedMotor baglås = new EV3MediumRegulatedMotor(MotorPort.A);
+				EV3MediumRegulatedMotor skovl = new EV3MediumRegulatedMotor(MotorPort.D);
+				
+			*/
+				
+				UnregulatedMotor hjul1 = new UnregulatedMotor(MotorPort.B);
+				UnregulatedMotor hjul2 = new UnregulatedMotor(MotorPort.C);
+				
+				UnregulatedMotor baglås = new UnregulatedMotor(MotorPort.A);
+				UnregulatedMotor skovl = new UnregulatedMotor(MotorPort.D);
+				
+				
+				hjul1.setPower(100);
+				hjul2.setPower(100);
+				skovl.setPower(100);
+				
+				
+				hjul1.backward();
+				hjul2.backward();
+				skovl.backward();
+				
+				Delay.msDelay(3000);
+				
+				hjul1.stop();
+				hjul2.stop();
+				skovl.stop();
+				
+				baglås.setPower(100);
+				baglås.backward();
+				Delay.msDelay(1500);
+				
+				baglås.forward();
+				Delay.msDelay(1000);
 
 	}
 }
